f185c9ba30ecdece7981ec99537909045a5495ad
malwarenights <malwarenights@users.noreply.github.com>
2017-05-24 17:27:20 +0200

    calling extra cmd.exe was waiting to exit - removing that one

--- vbox_bootstrap.bat
+++ vbox_bootstrap.bat
@@ -3,7 +3,7 @@ echo ==== Or Start / cmd / right click / Run as Administrator
 
 
 echo ==== Disable UAC
-%windir%\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+%windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
 
 
 echo ==== Mount the shares from the VirtualBox
