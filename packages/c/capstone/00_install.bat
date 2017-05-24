c3259f8e1f7fa2a33f00daaecc9312c2bfefa2b4
malwarenights <malwarenights@users.noreply.github.com>
2017-05-24 09:13:02 +0200

    adding bootstrap for people who have not go through malboxes installation

---
+++ packages/c/capstone/00_install.bat
@@ -0,0 +1,40 @@
+rem @echo off
+pushd "%~dp0"
+
+
+if "%SOFTWARE%"=="" (set SOFTWARE=C:\software)
+set TARGET="%SOFTWARE%\7zip"
+set PACKAGE32=7z1604.exe
+set PACKAGE64=7z1604-x64.exe
+
+if exist "%TARGET%" goto END
+
+@rem Add path of the 7zip directory to the system PATH
+reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v PATH /d "%PATH%;%TARGET%" /f
+
+@rem Check the architecture
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
+if %OS%==64BIT goto INSTALL64
+
+:INSTALL32
+echo ==== Installing %~dp0%PACKAGE32% to %TARGET%
+COPY %PACKAGE32% %TEMP%\%PACKAGE32%
+%TEMP%\%PACKAGE32% /S /D=%TARGET%
+DEL %TEMP%\%PACKAGE32%
+GOTO END
+
+
+
+:INSTALL64
+echo ==== Installing %~dp0%PACKAGE64% to %TARGET%
+COPY %PACKAGE64% %TEMP%\%PACKAGE64%
+%TEMP%\%PACKAGE64% /S /D=%TARGET%
+DEL %TEMP%\%PACKAGE64%
+GOTO END
+
+
+
+:END
+popd
+
+
--- vbox_bootstrap.bat
+++ vbox_bootstrap.bat
@@ -1,4 +1,68 @@
-echo ==== Right click, Run as Administrator
+echo ==== Right click in explorer, Run as Administrator
+echo ==== Or Start / cmd / right click / Run as Administrator
+
+
 echo ==== Disable UAC
 %windir%\System32\cmd.exe /k %windir%\System32\reg.exe ADD HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f
+
+
+echo ==== Mount the shares from the VirtualBox
+net use r: \\vboxsvr\share_ro
+net use w: \\vboxsvr\share_rw
+
+echo ==== Create couple of user identities
+net user admin01 admin01 /add
+net localgroup administrators admin01 /add
+net user admin02 admin02 /add
+net localgroup administrators admin01 /add
+
+net user power01 power01 /add
+net localgroup "power users" power01 /add
+net user power02 power02 /add
+net localgroup "power users" power02 /add
+
+net user user01 user01 /add
+net user user02 user02 /add
+net user user03 user03 /add
+net user user04 user04 /add
+net user user05 user05 /add
+
+echo ==== Autologin with one of the admin accounts
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v AutoAdminLogon /t REG_DWORD /d 0x1 /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultUserName /t REG_SZ /d "admin01" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v DefaultPassword /t REG_SZ /d "admin01" /f
+
+echo ==== Disable system restore on the system drive to save some space
+wmic /namespace:\\root\default path SystemRestore call Disable %SystemDrive%
+
+echo ==== Enable RDP in case it is not
+wmic rdtoggle where ServerName="%COMPUTERNAME%" CALL SetAllowTSConnections 1, 1
+
+echo ==== Disable automatic updates
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update" /v AUOptions /t REG_DWORD /d 1 /f
+
+echo ==== Configure kernel mode debugging
+bcdedit /copy {current} /d "Windows [debugger disabled]"
+bcdedit /debug {current} ON
+bcdedit /set {current} debugtype SERIAL
+bcdedit /set {current} debugport 1
+bcdedit /set {current} baudrate 115200
+
+echo ==== Enable users to RDP
+wmic rdpermissions where TerminalName="RDP-Tcp" CALL AddAccount "%USERNAME%",1
+
+echo ==== Tweak the file explorer
+rem ===== Configures Explorer to show hidden files, folders and drives
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v SuperHidden /t REG_DWORD /d 0x1 /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v SuperHidden /t REG_DWORD /d 0x1 /f
+rem ===== Configures Explorer to show protected operating system files
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 0x1 /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v ShowSuperHidden /t REG_DWORD /d 0x1 /f
+rem ===== Configures Explorer to show extensions of known files types
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0x0 /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /v HideFileExt /t REG_DWORD /d 0x0 /f
+rem ===== Configures Explorer to prevent windows from being automatically arranged when moved to the edge of the screen
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v FullPath /t REG_DWORD /d 0x1 /f
+reg add "HKLM\Software\Microsoft\Windows\CurrentVersion\Explorer\CabinetState" /v FullPath /t REG_DWORD /d 0x1 /f
+
 
