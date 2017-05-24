e34a3c162d283e9394405f45fcb3f20e36a9948b
malwarenights <malwarenights@users.noreply.github.com>
2017-05-24 22:44:31 +0200

    fixing downloads without known hash ... now all_downloads.txt finally looks like it should be

--- packages/s/sleuthkit/00_install.bat
+++ packages/s/sleuthkit/00_install.bat
@@ -3,20 +3,14 @@ pushd "%~dp0"
 
 
 if "%SOFTWARE%"=="" (set SOFTWARE=C:\software)
-set TARGET="%SOFTWARE%\sleuthkit-4.4.0-win32"
+set TARGET="%SOFTWARE%\sleuthkit-4.4.0"
 set PACKAGE32=sleuthkit-4.4.0-win32.zip
 
 
 if exist "%TARGET%" goto END
 
-@rem Check the architecture
-reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set OS=32BIT || set OS=64BIT
-if %OS%==32BIT goto INSTAL32
-
-:INSTALL32
 echo ==== Installing %~dp0%PACKAGE32% to %TARGET%
 7z x %PACKAGE32% -o%SOFTWARE%
-GOTO END
 
 
 
