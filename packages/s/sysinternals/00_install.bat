512d810c81f8581bf236cfe1dbf8ca104fa8e2d6
malwarenights <malwarenights@users.noreply.github.com>
2017-06-05 23:06:49 +0200

    adding the sysmon configuration from https://github.com/SwiftOnSecurity/sysmon-config

--- packages/s/sysinternals/00_install.bat
+++ packages/s/sysinternals/00_install.bat
@@ -12,6 +12,18 @@ if exist "%TARGET%" goto END
 echo ==== Installing %~dp0%PACKAGE% to %TARGET%
 7z x %PACKAGE% -o%TARGET%
 
+echo ==== Install the sysmon
+%TARGET%\sysmon.exe -accepteula -h md5,sha1,sha256,imphash -i -n
+
+echo ==== Update the configuration
+%TARGET%\sysmon.exe -c sysmonconfig-rebox.xml
+
+echo ==== Increase log size to 200 MB
+wevtutil sl Microsoft-Windows-Sysmon/Operational /ms:209715200
+
+echo ==== The events are written to Application and Services Logs/Microsoft/Windows/Sysmon/Operational log
+echo ==== (For earlier – unsupported – versions of Windows, it places those under the Windows System log).
+
 GOTO END
 :END
 popd
