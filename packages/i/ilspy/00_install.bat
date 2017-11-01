641991676004a839a17750cf01b184868fc2fb89
malwarenights <malwarenights@users.noreply.github.com>
2017-11-01 23:42:34 +0100

    update ilspy to current version

--- packages/i/ilspy/00_install.bat
+++ packages/i/ilspy/00_install.bat
@@ -5,7 +5,7 @@ if "%SOFTWARE%"=="" (set SOFTWARE=C:\software)
 set TARGET=%SOFTWARE%\ilspy
 if exist %TARGET% goto END
 
-7z x -o%TARGET% ILSpy_Master_2.4.0.1963_Binaries.zip
+7z x -o%TARGET% ILSpy_binaries_3.0.0.3142.zip
 
 :END
 popd
