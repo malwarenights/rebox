a31df4a24df78ae72ab9ce50a5763a84bc633dca
Michal Ambroz <xambroz@users.noreply.github.com>
2018-10-08 07:23:31 +0200

    update sleuthkit

--- packages/s/sleuthkit/00_install.bat
+++ packages/s/sleuthkit/00_install.bat
@@ -3,8 +3,8 @@ pushd "%~dp0"
 
 
 if "%SOFTWARE%"=="" (set SOFTWARE=C:\software)
-set TARGET=%SOFTWARE%\sleuthkit-4.4.0
-set PACKAGE32=sleuthkit-4.4.0-win32.zip
+set TARGET=%SOFTWARE%\sleuthkit-4.6.2
+set PACKAGE32=sleuthkit-4.6.2-win32.zip
 
 
 if exist "%TARGET%" goto END
