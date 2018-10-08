ce0293bb573fa0357128cc363f443843e328782e
Michal Ambroz <xambroz@users.noreply.github.com>
2018-10-08 03:48:04 +0200

    accept eula

--- packages/s/sysinternals/00_install.bat
+++ packages/s/sysinternals/00_install.bat
@@ -22,10 +22,16 @@ echo ==== Increase log size to 200 MB
 wevtutil sl Microsoft-Windows-Sysmon/Operational /ms:209715200
 
 echo ==== The events are written to Application and Services Logs/Microsoft/Windows/Sysmon/Operational log
-echo ==== (For earlier – unsupported – versions of Windows, it places those under the Windows System log).
+echo ==== (For earlier -unsupported- versions of Windows, it places those under the Windows System log).
+
 
 GOTO END
 :END
+
+echo ==== Accept EULA for the tools
+reg.exe ADD HKCU\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f
+reg.exe ADD HKU\.DEFAULT\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f
+
 popd
 
 
