61dfebeb54d5e8c204456f5a62c9242df51d22a9
rpmbuild <rpmbuild>
2019-09-10 17:40:54 +0200

    update sysinternals with version from 2019-09-05

--- packages/s/sysinternals/00_install.bat
+++ packages/s/sysinternals/00_install.bat
@@ -4,7 +4,7 @@ pushd "%~dp0"
 
 if "%SOFTWARE%"=="" (set SOFTWARE=C:\software)
 set TARGET=%SOFTWARE%\sysinternals
-set PACKAGE=SysinternalsSuite-20180724.zip
+set PACKAGE=SysinternalsSuite-20190905.zip
 
 
 if exist "%TARGET%" goto END
