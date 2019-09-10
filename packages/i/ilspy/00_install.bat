4e01fdc8d3b47f4128ee5d0407355eadf94295e0
rpmbuild <rpmbuild>
2019-09-10 17:30:59 +0200

    Update IlSpy

--- packages/i/ilspy/00_install.bat
+++ packages/i/ilspy/00_install.bat
@@ -2,10 +2,10 @@
 pushd "%~dp0"
 
 if "%SOFTWARE%"=="" (set SOFTWARE=C:\software)
-set TARGET=%SOFTWARE%\ilspy
+set TARGET="%SOFTWARE%\ilspy"
 if exist %TARGET% goto END
 
-7z x -o%TARGET% ILSpy_binaries_3.0.0.3142.zip
+7z x -o%TARGET% ILSpy_binaries_5.0.0.5106-rc1.zip
 
 :END
 popd
